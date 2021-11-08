d574b56898c18d4eefbcdc5bbdffcc261357a554
Juliana Ifionu <julieifionu@gmail.com>
2021-11-08 13:51:01 +0100

    Create public and private method

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(name = "Unknown", age, parent_permission = true)
+  def initialize(age, name = "Unknown", parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -24,4 +24,23 @@ class Person
   def age=(value)
     @age = value
   end
+
+  def can_use_service?
+    if is_of_age? || @parent_permission
+      true
+    else false
+    end
+  end
+
+  private
+  def is_of_age?
+    if @age >= 18
+      true
+    else false
+    end
+  end
+
 end
+
+person_1 = Person.new(18)
+p person_1.can_use_service?
