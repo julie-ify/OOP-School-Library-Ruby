05f31b7e9ad70264d9f8fa9148e724f229f5b5b9
Juliana Ifionu <julieifionu@gmail.com>
2021-11-10 19:25:05 +0100

    Add both ways relationship

--- student.rb
+++ student.rb
@@ -4,6 +4,7 @@ require './person'
 
 class Student < Person
   attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission = true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -11,6 +12,11 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
 
