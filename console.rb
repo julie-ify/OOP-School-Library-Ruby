ae7848cf7881f81a73907ab79661d7d2a8c2f42a
Juliana Ifionu <julieifionu@gmail.com>
2021-11-10 16:45:33 +0100

    Create has-many relationship between Classroom and Student

--- console.rb
+++ console.rb
@@ -1,16 +1,22 @@
 require './person'
 require './student'
 require './teacher'
+require './classroom'
 
 person = Person.new(18, 'Julianaifionu', false)
-person.can_use_services?
 
-student = Student.new(2, 'Rex')
-student.can_use_services?
-student.play_hooky
+first_student = Student.new(22, 'first_classroom', 'Rex')
+second_student = Student.new(21, 'first_classroom', 'Prince')
+third_student = Student.new(23, 'first_classroom', 'Rose')
 
 teacher = Teacher.new('Math', 'Muna')
-teacher.can_use_services?
 
-person.validate_name
-person
+
+first_classroom = Classroom.new("first_classroom")
+first_classroom.students
+first_classroom.add_student(first_student)
+first_classroom.add_student(second_student)
+first_classroom.add_student(third_student)
+
+first_classroom.students
+p first_classroom.students.map{ |student| student.name }
