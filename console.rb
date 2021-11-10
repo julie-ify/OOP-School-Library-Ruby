038f0402692b639bef4975db230183b7dfb743a5
Juliana Ifionu <julieifionu@gmail.com>
2021-11-10 17:20:30 +0100

    Create belongs-to relationship between Student and Classrom

--- console.rb
+++ console.rb
@@ -7,16 +7,30 @@ person = Person.new(18, 'Julianaifionu', false)
 
 first_student = Student.new(22, 'first_classroom', 'Rex')
 second_student = Student.new(21, 'first_classroom', 'Prince')
-third_student = Student.new(23, 'first_classroom', 'Rose')
+third_student = Student.new(23, 'second_classroom', 'Rose')
 
 teacher = Teacher.new('Math', 'Muna')
 
-
+# has-many relationship
 first_classroom = Classroom.new("first_classroom")
+second_classroom = Classroom.new("second_classroom")
 first_classroom.students
+second_classroom.students
 first_classroom.add_student(first_student)
 first_classroom.add_student(second_student)
-first_classroom.add_student(third_student)
+second_classroom.add_student(third_student)
 
 first_classroom.students
-p first_classroom.students.map{ |student| student.name }
+second_classroom.students
+first_classroom.students.map{ |student| student.name }
+first_classroom.students.last.classroom
+
+# belongs-to relationship
+first_student.classroom
+first_student.classroom = first_classroom
+second_student.classroom = first_classroom
+third_student.classroom = second_classroom
+first_student.classroom
+first_student.classroom.label
+third_student.classroom.label
+
