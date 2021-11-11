e5edacb287c83227b8f6044dbf5974ce5b6fb0e8
Juliana Ifionu <julieifionu@gmail.com>
2021-11-11 20:13:12 +0100

    Create all the Method for the UI

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@
 require './person'
 
 class Teacher < Person
-  def initialize(specialization, name = 'Unknown', parent_permission = true)
+  def initialize(specialization, age, name = 'Unknown', parent_permission = true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
